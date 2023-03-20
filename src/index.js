04908b8910e3dbc42c65283c47d057881cbba23d
Gosia Stepaniak <g.a.stepaniak@gmail.com>
2023-03-20 21:22:51 +0100

    update
    fetch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
+//importy
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
+//zmienne
 const DEBOUNCE_DELAY = 300;
+const searchBox = document.querySelector("#search-box");
+// const searchParams = new URLSearchParams({
+//     _name: name.official,
+
+// });
+
+//funkcja wyszukiwania nazwy
+function searchCountry() {
+    let name = searchBox.value;
+
+    if (name.length <= 1) {
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+    }
+    fetchCountries(name)
+};
+
+//fetch 
+function fetchCountries(name) {
+    return fetch(`https://restcountries.com/v3.1/name/${name}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log("data:" + data)
+        })
+        .catch(error => {
+           Notiflix.Notify.failure(`Oops, there is no country with that name.`)
+        })
+};
+    
+
+
+searchBox.addEventListener("input", debounce(searchCountry, DEBOUNCE_DELAY));   
