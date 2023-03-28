556277c39390e9097d022efcae33bca434883820
Gosia Stepaniak <g.a.stepaniak@gmail.com>
2023-03-28 13:25:54 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+'use strict'
 //importy
 import './css/styles.css';
 import debounce from 'lodash.debounce';
@@ -11,11 +12,28 @@ const countryInfo = document.querySelector(".country-info");
 const countryList = document.querySelector(".country-list");
 
 
-let informationAboutCountry = ({ name, capital, population, flags, languages }) => {
-    const languageList = languages.map(lang => lang.name).join(', ');
-    console.log(languageList)
-    
-}
+const informationAboutCountry = ({ name, capital, population, flags, languages }) => {
+    countryInfo.innerHTML = '';
+    countryList.innerHTML = '';
+    countryInfo.innerHTML = `<h1 class="country__header"><img src="${flags.svg}" alt="${name.common}" width="40" height="40">${name.common}</h1>
+    <p><b>Capital:</b> ${capital}</p>
+    <p><b>Population:</b> ${population}</p>
+    <p><b>Languages:</b> ${Object.values(languages).join(', ')}</p>`;
+};
+
+function foundCountriesList(countries) {
+  const countriesArray = countries.map(({ name, flags }) => {
+    const countryCard = document.createElement('div');
+    countryCard.classList.add('card');
+    countryCard.innerHTML = `<img src="${flags.svg}" width="40" height="40"><p class="country__name">${name.common}</p>`;
+
+    return countryCard;
+  });
+
+  countryList.innerHTML = '';
+  countryInfo.innerHTML = '';
+  countryList.append(...countriesArray);
+};
 
 //funkcja wyszukiwania nazwy
 function searchCountry() {
@@ -23,8 +41,8 @@ function searchCountry() {
     const nameOfCountry = searchName.trim();
 
     if (nameOfCountry.length === 0) {
-        // countryInfo.innerHTML = '';
-        // countryList.innerHTML = '';
+        countryInfo.innerHTML = '';
+        countryList.innerHTML = '';
         return
     }
     if (nameOfCountry.length <= 1) {
@@ -35,14 +53,14 @@ function searchCountry() {
             .then(countries => {
                 if (countries.length > 10) {
                     Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-                    console.log("więcej niż 10")
+                    console.log("więcej niż 10", countries.length)
                 }
                 if (countries.length === 1) {
-                    console.log("działa ,", countries.languages)
+                    return informationAboutCountry(countries[0]);
                 }
-                else {
-                    
-                }
+                if (countries.length > 1 && countries.length < 10) {
+                    return foundCountriesList(countries)
+                };
         })
     }
 };
